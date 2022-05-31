7dbc22e83436134c9336268855ee3ba8f24e195f
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-05-31 02:52:47 +0200

    Add files via upload

---
+++ S2HL.py
@@ -0,0 +1,10 @@
+import urllib
+import re
+from bs4 import BeautifulSoup
+import streamlit as st
+
+st.set_page_config(page_title="My Website", page_icon=":tada", layout="wide")
+st.subheader("Sensor 2")
+
+st.write("Sensor 2 in the basememt tunnel shaft control unit, has detetected humidity levels lower than the threshold of 0%")
+
